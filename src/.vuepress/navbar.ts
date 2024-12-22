c19deb039bb16d906a62acd9043357410baa3e7a
jason <327678749@qq.com>
2024-12-22 14:21:20 +0800

    update navbar;

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -3,37 +3,37 @@ import { navbar } from "vuepress-theme-hope";
 export default navbar([
   "/",
   "/demo/",
-  {
-    text: "文章",
-    icon: "pen-to-square",
-    prefix: "/posts/",
-    children: [
-      {
-        text: "苹果x",
-        icon: "pen-to-square",
-        prefix: "apple/",
-        children: [
-          { text: "苹果1", icon: "pen-to-square", link: "1" },
-          { text: "苹果2", icon: "pen-to-square", link: "2" },
-        ],
-      },
-      {
-        text: "香蕉",
-        icon: "pen-to-square",
-        prefix: "banana/",
-        children: [
-          {
-            text: "香蕉 1",
-            icon: "pen-to-square",
-            link: "1",
-          },
-          {
-            text: "香蕉 2",
-            icon: "pen-to-square",
-            link: "2",
-          }
-        ],
-      },
-    ],
-  },
+  // {
+  //   text: "文章",
+  //   icon: "pen-to-square",
+  //   prefix: "/posts/",
+  //   children: [
+  //     {
+  //       text: "苹果x",
+  //       icon: "pen-to-square",
+  //       prefix: "apple/",
+  //       children: [
+  //         { text: "苹果1", icon: "pen-to-square", link: "1" },
+  //         { text: "苹果2", icon: "pen-to-square", link: "2" },
+  //       ],
+  //     },
+  //     {
+  //       text: "香蕉",
+  //       icon: "pen-to-square",
+  //       prefix: "banana/",
+  //       children: [
+  //         {
+  //           text: "香蕉 1",
+  //           icon: "pen-to-square",
+  //           link: "1",
+  //         },
+  //         {
+  //           text: "香蕉 2",
+  //           icon: "pen-to-square",
+  //           link: "2",
+  //         }
+  //       ],
+  //     },
+  //   ],
+  // },
 ]);
